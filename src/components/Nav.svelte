2330b14184fdfb03249364170f7f0648fbc8021c
albert <albertmasa2@gmail.com>
2020-09-13 17:38:05 +0200

    feat: add translation to route names

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-  import {
-    Header,
-    HeaderNav,
-    HeaderNavItem,
-    HeaderNavMenu,
-  } from "carbon-components-svelte";
+  import { Header, HeaderNav, HeaderNavItem } from "carbon-components-svelte";
+  import { _ } from "svelte-i18n";
 
   $: getAriaCurrent = (href?: string) =>
     segment === href ? "page" : undefined;
 
-  export let segment;
+  export let segment: string;
+
+  const routes = [
+    "campaigns",
+    "mission",
+    "composers",
+    "shop",
+    "feedback",
+    "contact",
+  ];
 </script>
 
-<Header platformName="Newold Music">
+<style>
+  .logo {
+    height: 100%;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    margin-right: 2rem;
+  }
+
+  :global(.bx--header) {
+    height: 4rem;
+  }
+</style>
+
+<Header platformName="">
+  <img class="logo" src="logo-dark-192.png" alt="Logo" />
   <HeaderNav>
-    <HeaderNavItem href="." text="Home" aria-current={getAriaCurrent()} />
     <HeaderNavItem
-      href="projects"
-      text="Projects"
-      aria-current={getAriaCurrent('projects')} />
-    <HeaderNavItem
-      href="shop"
-      text="Shop"
-      aria-current={getAriaCurrent('shop')} />
+      href="."
+      text={$_('routes.home')}
+      aria-current={getAriaCurrent()} />
+
+    {#each routes as route}
+      <HeaderNavItem
+        href={route}
+        text={$_(`routes.${route}`)}
+        aria-current={getAriaCurrent(route)} />
+    {/each}
   </HeaderNav>
 </Header>
