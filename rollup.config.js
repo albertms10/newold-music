707403cc6874cafdeaa64c2d222fd7a89d9be63a
albert <albertmasa2@gmail.com>
2020-09-13 17:34:55 +0200

    feat: add json parsing to Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import svelte from "rollup-plugin-svelte";
@@ -39,6 +40,7 @@ export default {
         dedupe: ["svelte"],
       }),
       commonjs(),
+      json(),
       typescript(),
 
       legacy &&
@@ -93,6 +95,7 @@ export default {
         dedupe: ["svelte"],
       }),
       commonjs(),
+      json(),
       typescript(),
     ],
     external: Object.keys(pkg.dependencies).concat(
