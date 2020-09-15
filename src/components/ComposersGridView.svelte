822ed8b149f6e7757d89d4f69f1a7a557e869936
albert <albertmasa2@gmail.com>
2020-09-15 16:26:35 +0200

    feat: add composers GridView

---
+++ src/components/ComposersGridView.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { ApolloQueryResult } from "apollo-boost";
+  import { Loading } from "carbon-components-svelte";
+  import { query, restore } from "svelte-apollo";
+  import GridView from "../components/GridView.svelte";
+  import { client } from "../database/apollo";
+  import type { Composer } from "../database/model";
+  import { COMPOSER_LIST } from "../database/queries";
+
+  export let composerCache: ApolloQueryResult<Composer>;
+
+  restore(client, COMPOSER_LIST, composerCache.data);
+
+  const composers = query<{ composer: Composer[] }>(client, {
+    query: COMPOSER_LIST,
+  });
+</script>
+
+{#await $composers}
+  <Loading small />
+{:then { data }}
+  <GridView
+    items={data.composer.map(({ id, name, surname }) => ({
+      id,
+      title: `${name} ${surname}`,
+    }))} />
+{/await}
--- src/routes/composers.svelte
+++ src/routes/composers.svelte
@@ -1,5 +1,21 @@
+<script context="module">
+  import { client } from "../database/apollo";
+  import { COMPOSER_LIST } from "../database/queries";
+
+  export async function preload() {
+    return {
+      composerCache: await client.query({ query: COMPOSER_LIST }),
+    };
+  }
+</script>
+
 <script lang="ts">
+  import type { ApolloQueryResult } from "apollo-boost";
   import { _ } from "svelte-i18n";
+  import ComposersGridView from "../components/ComposersGridView.svelte";
+  import type { Composer } from "../database/model";
+
+  export let composerCache: ApolloQueryResult<Composer>;
 </script>
 
 <svelte:head>
@@ -7,3 +23,5 @@
 </svelte:head>
 
 <h1>{$_('routes.composers')}</h1>
+
+<ComposersGridView bind:composerCache />
