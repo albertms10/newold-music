81a27615e787307ca59d3dc94066cf909107fa22
albert <albertmasa2@gmail.com>
2020-09-15 16:26:13 +0200

    feat: add GridView

---
+++ src/components/GridView.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  interface GridViewItem {
+    id: number | string;
+    title: string;
+    meta?: string;
+  }
+
+  export let items: GridViewItem[];
+</script>
+
+<style>
+  section {
+    display: flex;
+  }
+
+  article {
+    background-color: #fff;
+  }
+</style>
+
+<section>
+  {#each items as { id, title, meta } (id)}
+    <article>
+      <h3>{title}</h3>
+      {#if meta}
+        <p>{meta}</p>
+      {/if}
+    </article>
+  {/each}
+</section>
