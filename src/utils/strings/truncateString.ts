f72d1e6a64c7df6d75773eadf913f55db0094969
albert <albertmasa2@gmail.com>
2020-09-18 02:04:35 +0200

    docs: add JSDoc comment description to truncateString

--- src/utils/strings/truncateString.ts
+++ src/utils/strings/truncateString.ts
@@ -1,5 +1,8 @@
 // Gist: https://gist.github.com/DylanAttal/13716cfd9272e92a544ddddde221eab1
 
+/**
+ * Truncates the given string to the number of characters with a trailing ellipsis (if needed).
+ */
 const truncateString = (str: string, num: number) => {
   if (num >= str.length) return str;
 
