c66c08fb37ad4c52323a14cb3ab1e43a4c400850
albert <albertmasa2@gmail.com>
2020-09-29 16:37:43 +0200

    refactor: change misson route dir

---
+++ src/routes/mission/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Accordion, AccordionItem } from "carbon-components-svelte";
+  import { _ } from "svelte-i18n";
+  import { faqs } from "./_faqs.json";
+</script>
+
+<style>
+  h1,
+  h2 {
+    margin-bottom: 2rem;
+  }
+</style>
+
+<svelte:head>
+  <title>{$_('routes.mission')} — Newold Music</title>
+</svelte:head>
+
+<h1>{$_('routes.mission')}</h1>
+
+<h2 id="faq">Preguntes freqüents</h2>
+
+<Accordion>
+  {#each faqs as { id, q, a } (id)}
+    <AccordionItem title={q}>{a}</AccordionItem>
+  {/each}
+</Accordion>
