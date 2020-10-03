ac0dc394e037d2b75ca4a91a8a19e166316fd6e6
albert <albertmasa2@gmail.com>
2020-10-03 14:21:57 +0200

    feat: add Contact form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+  import {
+    Button,
+    Form,
+    FormGroup,
+    TextArea,
+    TextInput,
+  } from "carbon-components-svelte";
   import { _ } from "svelte-i18n";
+  import { getFormData } from "utils/misc";
+
+  const handleSubmit = (e: Event) => {
+    const data = getFormData(e.target as HTMLFormElement);
+  };
 </script>
 
 <style>
@@ -13,3 +25,21 @@
 </svelte:head>
 
 <h1>{$_('routes.contact')}</h1>
+
+<Form on:submit={handleSubmit}>
+  <FormGroup>
+    <TextInput name="name" labelText={$_('fields.name')} required />
+  </FormGroup>
+  <FormGroup>
+    <TextInput
+      name="email"
+      type="email"
+      labelText={$_('fields.email')}
+      required
+    />
+  </FormGroup>
+  <FormGroup>
+    <TextArea name="message" rows={3} labelText={$_('fields.message')} />
+  </FormGroup>
+  <Button type="submit">{$_('actions.send')}</Button>
+</Form>
