f4eeaabe8ca1251a1a9cf0a15bd06c2214b86b7c
albert <albertmasa2@gmail.com>
2020-10-25 22:13:26 +0100

    feat(apollo): change ApolloClient config with WebSocket

--- src/database/apollo.ts
+++ src/database/apollo.ts
@@ -1,12 +1,53 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client/core";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import fetch from "cross-fetch";
+import ws from "ws";
+
+export const HASURA_GRAPHQL_ENGINE_HOSTNAME = "premium-turtle-50.hasura.app";
+
+const scheme = (proto: string) =>
+  process.env.BACKEND_SSL ? `${proto}s` : proto;
+
+const wsurl = `${scheme("ws")}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
+const httpurl = `${scheme(
+  "http"
+)}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
+
+const wsLink = new WebSocketLink({
+  uri: wsurl,
+  options: {
+    reconnect: true,
+  },
+  webSocketImpl: ws,
+});
+
+const httpLink = new HttpLink({
+  uri: httpurl,
+  credentials: "include",
+  fetch,
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const createApolloClient = () =>
   new ApolloClient({
-    link: new HttpLink({
-      uri: "https://premium-turtle-50.hasura.app/v1/graphql",
-      fetch,
-    }),
+    link,
     cache: new InMemoryCache(),
   });
 
